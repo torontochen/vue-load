42140e3f913d531d2c89dece1c6edcc5e744b599
Jack Lee <jack@hotmail.com>
2019-08-11 23:35:15 -0400

    main-ws link

--- src/main.js
+++ src/main.js
@@ -2,7 +2,110 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
+import './plugins/vuetify'
+import {
+  ApolloClient
+} from "apollo-client"
+import {
+  InMemoryCache
+} from "apollo-cache-inmemory"
+import {
+  CreateUploadLink
+} from "apollo-upload-client"
+import {
+  OnError,
+  onError
+} from "apollo-link-error"
+import {
+  ApolloLink,
+  Observable
+} from "apollo-link"
+import {
+  WebSocketLink,
+} from "apollo-link-ws"
+import {
+  GetMainDefinition
+} from "apollo-utilities"
+import VueApollo from "vue-apollo"
+
+
+// Register Global Component
+Vue.use(VueApollo)
+
+//Set up request
+const request = operation => {
+  // if no token with key in the localStorage, add it
+  if (!localStorage.token) {
+    localStorage.setItem("token", '')
+  }
+
+  //operation adds the token to an authorization header that is to be sent to server
+  operation: setContext({
+    headers: {
+      authorization: localStorage.getItem('token')
+    }
+  })
+}
+
+//set up the request handlers for the http clients
+const requestLink = new ApolloLink((operation, forward) => {
+  return new Observable(observer => {
+    let handle
+    Promise.resolve(operation)
+      .then(oper => {
+        request(oper)
+      })
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer)
+        })
+      })
+      .catch(observer.error.bind(observer))
+    return () => {
+      if (handle) handle.unsubscribe()
+    }
+  })
+})
+
+// websocket link for subscriptions
+const wsLink = ApolloLink.from([
+  onError(({
+    graphQLErrors,
+    networkError
+  }) => {
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        console.log(`[GraphQL error]: Message: ${err.message},Location: ${err.locations}, Path: ${err.path}`)
+        if (err.name === 'AuthenticationError') {
+          console.log(err)
+
+        }
+      }
+    }
+    if (networkError) {
+      console.log("[networkError]", networkError)
+    }
+  }),
+
+  requestLink,
+
+  new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams: () => {
+        if (localStorage.token) {
+          const token = localStorage.getItem('token')
+          return {
+            Authorization: token
+          }
+        }
+      }
+    }
+  })
+])
 
 Vue.config.productionTip = false
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.use(Vuetify)
-
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi'
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#616161",
+    secondary: "#BDBDBD",
+    info: "#A1887F",
+    error: "#EF5350",
+    warning: "#FF7043",
+    accent: "#6D4C41",
+    success: "#388E3C"
   }
-})
+});
